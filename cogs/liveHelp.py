672104f69cebd8df9621551c13ff6702cbac88e0
Animus <lovro@x-network.io>
2022-07-11 06:38:56 +0300

    thread creator

---
+++ cogs/liveHelp.py
@@ -0,0 +1,83 @@
+"""
+COG: Handles the settings for communities verification system from bot invasion
+"""
+import nextcord
+from nextcord import Interaction, slash_command
+from nextcord.channel import ChannelType
+from nextcord.ext import commands
+from colorama import init
+
+init(autoreset=True)
+
+MESSAGE_TO_LONG = "You message is to long. Allowed is maximum of 300 characters "
+
+
+class Helpers(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @slash_command(name="help", description="Open help thread with Staff", dm_permission=False, guild_ids=[])
+    @commands.has_permissions(create_public_threads=True)
+    async def urgent_mng(self, interaction: Interaction,*,reason):
+        thread = await interaction.channel.create_thread(name=f"@{interaction.user}-{interaction.user.id}",
+                                                         reason='Help Wanted',
+                                                         type=ChannelType.public_thread)
+
+        animus = self.bot.get_user(360367188432912385)
+        role = interaction.guild.get_role(785818459228995584)
+        await thread.add_user(animus)
+        await thread.add_user(self.bot.user)
+        await thread.send(
+            content=f"{interaction.user.mention} Please stay put. One of the X-Cash Staff or users with {role.mention} will be with"
+                    f" you as soon as possible in order to resolve the issue at hand.\n"
+                    f"Reason: ***{reason}***")
+        await interaction.response.send_message(content=f'{interaction.user.mention} You have successfully opened '
+                                                        f'up a thread. It can be access through link {thread.jump_url}',
+                                                ephemeral=True, delete_after=360)
+
+    @slash_command(name="discussion", description="Open up discussion (special-area)", dm_permission=False,guild_ids=[])
+    @commands.has_permissions(create_public_threads=True)
+    async def help_mng(self, interaction: Interaction, *, topic):
+        thread = await interaction.channel.create_thread(name=f"{topic}",
+                                                         reason='Discussion topic',
+                                                         type=ChannelType.public_thread)
+
+        animus = self.bot.get_user(360367188432912385)
+        role = interaction.guild.get_role(785818459228995584)
+        await thread.add_user(animus)
+        await thread.add_user(self.bot.user)
+        await thread.send(
+            content=f"{role.mention} {interaction.user.mention} has started a thread topic {topic}")
+        await interaction.response.send_message(content=f'{interaction.user.mention} You have successfully opened '
+                                                        f'up a thread. It can be access through link {thread.jump_url}',
+                                                ephemeral=True, delete_after=360)
+
+    @slash_command(name=f'close', description="Close the thread", dm_permission=False,guild_ids=[])
+    async def close_thread(self, interaction: Interaction):
+        if isinstance(interaction.channel, nextcord.Thread):
+            thread = interaction.guild.get_thread(interaction.channel.id)
+
+            # Get user ID from thread name
+            thread_name = thread.name
+            pos = thread_name.index("-")
+            owner_user_id = thread_name[pos + 1:]
+            guild = interaction.guild
+            role = guild.get_role(785818459228995584)
+            all_role_members = [x.id for x in role.members]
+
+            # This breaks if its not a help thread
+            if interaction.user.id == int(
+                    owner_user_id) or interaction.user.id in all_role_members or interaction.user.id == 360367188432912385:
+                await thread.delete()
+            else:
+                await interaction.response.send_message(content=f'{interaction.user.mention} You are not allowed to '
+                                                                f'close this thread. Only the user or members with role'
+                                                                f' @contributors can do so', ephemeral=True,
+                                                        delete_after=30)
+        else:
+            await interaction.response.send_message(content=f'This command works only in thread', ephemeral=True,
+                                                    delete_after=30)
+
+
+def setup(bot):
+    bot.add_cog(Helpers(bot))
